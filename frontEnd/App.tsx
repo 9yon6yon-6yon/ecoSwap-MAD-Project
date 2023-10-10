dab420a374557fb52c60124861067543568004a0
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-10-10 23:21:00 +0600

    first commit

---
+++ frontEnd/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+const App = () =>{
+ return(
+  <View style={styles.mainContainer}>
+    <Text style={styles.textContainer}>Hello World! ওহে দুনিয়া </Text>
+  </View>
+ )
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textContainer: {
+    fontWeight: "700",
+    fontSize: 16,
+    color: "red"
+  }
+})
+
+export default App;
