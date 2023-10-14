2af6f212739d0dca0bcad4c43fb81fe2409812bd
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-10-14 10:46:40 +0600

    navigation and useState practice

--- frontEnd/App.tsx
+++ frontEnd/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import CounterPage from "./src/pages/CounterPage";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginPage from "./src/pages/LoginPage";
 
-const App = () =>{
- return(
-  <View style={styles.mainContainer}>
-    <Text style={styles.textContainer}>Hello World! ওহে দুনিয়া </Text>
-  </View>
- )
+const stack = createStackNavigator();
+const App = () => {
+  return (
+  // <View style={styles.mainContainer}>
+  //   <Text style={styles.textContainer}>Hello World! ওহে দুনিয়া </Text>
+  // </View>
+
+  <NavigationContainer>
+    <stack.Navigator>
+        <stack.Screen name = "Counter" component={CounterPage}/>
+        <stack.Screen name = "Login" component={LoginPage}/>
+    </stack.Navigator>
+    </NavigationContainer>
+
+
+
+  // <CounterPage/>
+  )
 }
 //comment
 const styles = StyleSheet.create({
---
+++ frontEnd/src/pages/LoginPage.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+
+    console.log('Username: ', username);
+    console.log('Password: ', password);
+
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Username:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+      />
+      <Text style={styles.label}>Password:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
+
+export default LoginPage;
