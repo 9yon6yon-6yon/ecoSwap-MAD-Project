936e3eb1b8a8d71abbfbcfa142329718c15dd600
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-10-26 13:28:15 +0600

    edited pages

--- frontEnd/App.tsx
+++ frontEnd/App.tsx
@@ -2,11 +2,31 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SignUp from "./src/pages/SignUp";
+import LoginPage from "./src/pages/LoginPage";
 
 
 const stack = createStackNavigator();
+const bottom_tab = createBottomTabNavigator();
+
+const IndexPage = () =>{
+  return (
+    <stack.Navigator screenOptions={{ headerShown:false }}>
+     <stack.Screen name="Index" component={LoginPage} /> 
+     <stack.Screen name="Register" component={SignUp} />   
+    </stack.Navigator>
+  )
+}
+
 const App = () => {
-  return
+  return(
+    <NavigationContainer>
+      <bottom_tab.Navigator> 
+      <bottom_tab.Screen  name="Dashboard" component={IndexPage} />
+      </bottom_tab.Navigator> 
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ frontEnd/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+const index = () => {
+    return (
+        <View>
+            <Text>
+                index page
+            </Text>
+        </View>
+    )
+}
+
+export default index;
