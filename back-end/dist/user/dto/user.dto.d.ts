6311c5c6b51324be56736212de45bb178b305a8a
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-11-26 01:05:06 +0600

    backend problem

--- back-end/dist/user/dto/user.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class UserCredDTO {
-    readonly email: string;
-    readonly password: string;
-}
--- back-end/dist/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserCredDTO } from './dto/user.dto';
-import { UserService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    getUser(): string;
-    userLogin(userCredential: UserCredDTO): any;
-}
